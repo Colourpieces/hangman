a79b8b72dff95b9c19ac1195cd9e32daf44f8a15
nb <github@n-baller.de>
2023-06-29 21:18:15 +0200

    refactoring styling

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ let fails = 0;
 const failsTotal = 1;
 let gameStatus = "active";
 
-const secretWords = ["flexbox", "margin", "javascript", "array"];
+const secretWords = [
+  "flexbox",
+  "margin",
+  "javascript",
+  "array",
+  "padding",
+  "border",
+  "object",
+];
 const keyboard = document.querySelector("#keyboard");
 keyboard.addEventListener("click", (e) => {
   checkLetter(e.target);
@@ -94,10 +102,45 @@ function checkGameStatus() {
   } else if (!word.includes("_") && fails < failsTotal) {
     gameStatus = "win";
   } else {
-    //fails >= 10
+    //fails >= failsTotal
     gameStatus = "loose";
+
+    dropBody();
+    // $("#rEyes").addClass("hide");
+    // $("#xEyes").removeClass("hide");
   }
 }
 
 renderKeys();
 newGame();
+
+/*-----------------------*/
+// $("#animate").click(function(){
+//   dropBody();
+//   $("#rEyes").addClass("hide");
+//   $("#xEyes").removeClass("hide");
+// });
+
+/*
+function dropBody() {
+  $("#door1").velocity({ rotateZ: 90 }, 1000);
+  $("#door2").velocity({ rotateZ: -90 }, 1000);
+  fall();
+}
+
+function fall() {
+  let dur = 500;
+  let del = 1000;
+  $("#body").velocity({ translateY: "200px" }, { duration: dur, delay: del });
+  $("#rope").velocity({ y2: "+=200px" }, { duration: dur, delay: del });
+  $("#armL").velocity({ y2: "-=60px" }, { duration: dur, delay: del });
+  $("#armR").velocity({ y2: "-=60px" }, { duration: dur, delay: del });
+
+  finish();
+}
+
+function finish() {
+  $("#armL").velocity({ y2: "+=70px", x2: "+=10px" }, 500);
+  $("#armR").velocity({ y2: "+=70px", x2: "-=10px" }, 500);
+}
+*/
